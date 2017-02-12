b630826314fae3a01a36177221acfc7e7c60fe2c
Michael Hillcox <hello.michaelhillcox@gmail.com>
2017-02-12 16:06:35 +0000

    Improved favicon

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -4,6 +4,12 @@
         <meta charset="utf-8">
         <title>Michael Hillcox</title>
         <link rel="stylesheet" href="app/assets/master.min.css" media="screen" title="no title" charset="utf-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/png" href="app/assets/images/favicon-16x16.png" sizes="16x16">
+        <link rel="icon" type="image/png" href="app/assets/images/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="app/assets/images/favicon-96x96.png" sizes="96x96">
+
     </head>
     <body>
         <div id="container">
