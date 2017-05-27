c85387f280a13ee15cdc81d5cea486a7bda17ca9
Michael Hillcox <hello.michaelhillcox@gmail.com>
2017-05-27 17:20:46 +0100

    Some animations

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,7 @@
 $(document).ready((e) => {
+    var storage = window.localStorage,
+        first = storage.getItem("firsttime");
+
     var nav = $('#topNav'),
         navBtn = $('#navIcon'),
         close = nav.find('.close');
@@ -12,4 +15,15 @@ $(document).ready((e) => {
             nav.removeAttr("style");
         });
     });
+
+    setTimeout(function () {
+        $('body, html').animate({opacity: 1}, 300);
+        if( first === null )
+            $('#header').animate({top: 0}, 600, 'ease');
+        else
+            $('#header').css({top: 0});
+    }, 100);
+
+    if( first === null )
+        storage.setItem("firsttime", true);
 });
